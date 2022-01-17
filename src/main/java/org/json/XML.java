d5f0a608f9b57f0d8f129a7708a51b36bc21d607
Justin Grant <justingrant99@me.com>
2022-01-16 19:32:30 -0800

    still works

--- src/main/java/org/json/XML.java
+++ src/main/java/org/json/XML.java
@@ -241,6 +241,19 @@ public class XML {
         }
     }
 
+
+
+
+    public static boolean isInt(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(Exception e){
+            return false;
+        }
+
+    }
+
     /**
      * Scan the content following the named tag, attaching it to the context.
      *
@@ -331,6 +344,7 @@ public class XML {
             if (x.nextToken() != GT) {
                 throw x.syntaxError("Misshaped close tag");
             }
+//          MYCODEHERE
 //          Note if we have reached our object in question and we have reached the end of that said object
 //          then we can print out the context and be done with it
             if(done && token.equals(lastKey)){
@@ -345,47 +359,58 @@ public class XML {
             // Open tag <
 
         } else {
+//           MYCODEHERE
 //           START
 //           NOTE: this is where the Tagnames are
             tagName = (String) token;
+//           Maybe we can try to check if it's a number and if the keypath is a number we know it's an array
+//           otherwise we can just treat it as it were normally
+
 //           WE CAN DO SOME WORK HERE: check the tagName to see if it's the correct tagname
             String[] myKeys = keyPath.split("/");
-            if(!myKeys[0].equals("")){
+//          if we're indexing an array
+
+                if(!myKeys[0].equals("")){
 //              this means we are on the correct path so far
-                if(tagName.equals(myKeys[0])){
+                    if(tagName.equals(myKeys[0])){
 //                   then we need to check if this is the last key, if so, then we need to set done to true
 //                    to signify that we have reached our object
 //                  we have found the beginning of the key
-                    if(tagName.equals(lastKey)){
-                        done = true;
-                    } else {
+                        if(tagName.equals(lastKey)){
+                            done = true;
+                        } else {
 //                       this is where we iterate to the next key. We haven't found it but we're going to the next
 //                       key
-                        keyPath = "";
+                            keyPath = "";
+                            for(int j = 1; j < myKeys.length - 1; j++){
+                                keyPath += myKeys[j] + '/';
+                            }
+                            keyPath += myKeys[myKeys.length - 1];
+
+
+                        }
+                    }
+                } else{
+//              the first character is always / so when we split it's going to give us a blank at the beginning
+//              so we need to get past that
+                    keyPath = "";
+                    if(tagName.equals(myKeys[1])){
+                        for(int j = 2; j < myKeys.length - 1; j++){
+                            keyPath += myKeys[j] + '/';
+                        }
+                    } else {
                         for(int j = 1; j < myKeys.length - 1; j++){
                             keyPath += myKeys[j] + '/';
                         }
-                        keyPath += myKeys[myKeys.length - 1];
                     }
+                    keyPath += myKeys[myKeys.length - 1];
                 }
-            } else{
-//              the first character is always / so when we split it's going to give us a blank at the beginning
-//              so we need to get past that
-                keyPath = "";
-                if(tagName.equals(myKeys[1])){
-                    for(int j = 2; j < myKeys.length - 1; j++){
-                        keyPath += myKeys[j] + '/';
-                    }
-                } else {
-                    for(int j = 1; j < myKeys.length - 1; j++){
-                        keyPath += myKeys[j] + '/';
-                    }
-                }
-                keyPath += myKeys[myKeys.length - 1];
-            }
-            if(tagName.equals("whatever")){
-                done = true;
-            }
+            myKeys = keyPath.split("/");
+
+
+
+
+
             token = null;
             jsonObject = new JSONObject();
             boolean nilAttributeFound = false;
